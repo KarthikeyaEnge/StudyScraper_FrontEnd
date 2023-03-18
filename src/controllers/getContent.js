3ea1162ce835d0dc621202dbcdd6df92f71b4576
Karthikeya Enge <karthikeya.enge@hotmail.com>
2023-03-19 00:11:33 +0530

    updated code

---
+++ src/controllers/getContent.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const getContent = async (image) => {
+  const formData = new FormData();
+  formData.append("image", image);
+  formData.append("content-type", "image/jpeg");
+  let res = await axios.post("http://127.0.0.1:3500/datacall", formData, {
+    "Content-Type": "multipart/form-data",
+  });
+  console.log(res.data);
+};
+
+export default getContent;
